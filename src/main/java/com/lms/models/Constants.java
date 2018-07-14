756aa1408d0b919128495c24c3a04157595cbc2f
Nitin Birdi <nitin.birdi@naukri.com>
2018-07-14 22:14:35 +0530

    class for constants

---
+++ src/main/java/com/lms/models/Constants.java
@@ -0,0 +1,8 @@
+package com.lms.models;
+
+public class Constants {
+
+	public final static Integer MAX_BOOKS_ALLOWED = 2;
+	public final static Character PLUS = '+';
+	public final static Character MINUS = '-';
+}
