f3114f7bce99f90aa60bcee5d78ccd41ebf7fa71
Nitin Birdi <nitin.birdi@naukri.com>
2018-07-14 22:11:05 +0530

    validation messages

---
+++ src/main/java/com/lms/models/ValidationMessages.java
@@ -0,0 +1,8 @@
+package com.lms.models;
+
+public class ValidationMessages {
+
+	public static final String MAX_ISSUE_LIMIT = "Maximum issue limit of user has reached";
+	public static final String BOOK_NOT_AVAILABLE = "The book is currently not available";
+	public static final String BOOK_ALREADY_ISSUED = "This book has already been issued to the current user";
+}
