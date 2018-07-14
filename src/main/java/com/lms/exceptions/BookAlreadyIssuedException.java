d39df2c0a9fc7e401adbd87858b6ce36fd3d2a6b
Nitin Birdi <nitin.birdi@naukri.com>
2018-07-14 21:19:01 +0530

    adding exception classes

---
+++ src/main/java/com/lms/exceptions/BookAlreadyIssuedException.java
@@ -0,0 +1,10 @@
+package com.lms.exceptions;
+
+public class BookAlreadyIssuedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/lms/exceptions/BookNotAvailableException.java
@@ -0,0 +1,10 @@
+package com.lms.exceptions;
+
+public class BookNotAvailableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
