ed9d86c75487cc15422db365e983e2d0e8aa7e1b
Nitin Birdi <nitin.birdi@naukri.com>
2018-07-14 22:15:07 +0530

    model for book

---
+++ src/main/java/com/lms/models/Book.java
@@ -0,0 +1,92 @@
+package com.lms.models;
+
+/**
+ * 
+ * @author nitin
+ *
+ *1. Ability to add books to the system.
+2. Ability to add users to the system.
+3. Ability to lend books to users.
+4. Ability to return books to the library.
+5. Ability to limit the number of books borrowed by user.
+6. Ability to search a book by title, author.
+7. Ability to search a user by name.
+ */
+
+public class Book {
+
+	private String name;
+	private String author;
+	
+	public Book(String name, String author) {
+		super();
+		this.name = name;
+		this.author = author;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
